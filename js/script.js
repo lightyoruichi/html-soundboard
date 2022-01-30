400092c7b2ce8e71dad117b3cf372931f3a22b68
Harinder Singh <satnam182@gmail.com>
2022-01-31 01:33:03 +0800

    fix
    upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function(){
+	// Remove no-javascript class if js is enabled
+	$("body.no-javascript").removeClass("no-javascript")
+
+	$("audio").removeAttr("controls").each(function(i, audioElement) {
+	    var audio = $(this);
+	    var that = this;
+	    $("#doc").append($('<button class="btn '+audio.attr("class")+'" title="'+audio.attr("title")+'">'+audio.attr("title")+'</button>').click(function() {that.play();}));
+	});
+
+
+$("audio").bind("play",function (){
+  $("audio").not(this).each(function (index, audio) {
+    audio.pause();
+  });
+});
+
+});
+
+
+
+
+
